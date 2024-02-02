b34882fc51dc195f2cb56b642543c6dcb8be87ce
nopnapatn <nopnapatn@gmail.com>
2024-02-02 23:10:08 +0700

    feat(controllers): add places-controller to handle get place, user

--- routes/places-route.js
+++ routes/places-route.js
@@ -1,46 +1,8 @@
 const express = require("express")
 const router = express.Router()
-const HttpError = require("./models/http-error")
+const placesController = require("../controllers/places-controller")
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-]
-
-router.get("/:pid", (req, res, next) => {
-  //   console.log("GET Request in Places")
-  const placeId = req.params.pid
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId
-  })
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404)
-  }
-
-  res.json({ place })
-})
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId
-  })
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided user id.", 404)
-  }
-
-  res.json({ place })
-})
+router.get("/:pid", placesController.getPlaceById)
+router.get("/user/:uid", placesController.getPlaceByUserId)
 
 module.exports = router
